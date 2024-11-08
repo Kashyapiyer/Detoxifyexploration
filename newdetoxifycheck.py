c7e88a6e6f93e1753d58bdb3270ae3ce4cb8a197
K <k.iyer2611@gmail.com>
2024-11-08 13:42:50 -0600

    Update newdetoxifycheck.py

--- newdetoxifycheck.py
+++ newdetoxifycheck.py
@@ -14,14 +14,14 @@ pd.set_option('display.max_colwidth', None)
 
 
 
-
 def predictsentencetoxicity(texts_col, modelname='unbiased'):
     if texts_col is not None and bool(texts_col.strip()):
         detox_model = Detoxify(modelname)
         result = detox_model.predict(str(texts_col))
         sorted_result = dict(sorted(result.items(), key=lambda x: x[1], reverse=True))
         maxoftwo = dict(Counter(sorted_result).most_common(2))        
-        avgofmaxtwopercentage = (sum(maxoftwo.values()) / len(maxoftwo) * 100)
+        #avgofmaxtwopercentage = (sum(maxoftwo.values()) / len(maxoftwo) * 100)
+        avgofmaxtwopercentage = ((sum(maxoftwo.values()) / len(maxoftwo)) * 100)
         toxicityeval = 1 if avgofmaxtwopercentage > 90 else 0
         result['question'] = texts_col
         result['maxoftwo'] = maxoftwo
@@ -33,12 +33,11 @@ def predictsentencetoxicity(texts_col, modelname='unbiased'):
 def calculatetoxicity(df, texts_col, modelname='unbiased'):
     detox_model = Detoxify(modelname)
     df['toxic_result'] = dict(df['text'].apply(lambda t: detox_model.predict(str(t))))
-
-    df['maxoftwo'] = df['toxic_result'].apply(lambda x: dict(sorted(Counter(x).most_common(2), key=lambda item: item[1], reverse=True)))
-    
+    df['maxoftwo'] = df['toxic_result'].apply(lambda x: dict(sorted(Counter(x).most_common(2), key=lambda item: item[1], reverse=True))
     #df['maxoftwo'] = df['toxic_result'].apply(lambda x: dict(Counter(x).most_common(2)))
     maxoftwo  = df['maxoftwo'][0]
-    df['avgofmaxtwopercentage']= (sum(maxoftwo.values()) / len(maxoftwo) * 100)
+    #df['avgofmaxtwopercentage']= (sum(maxoftwo.values()) / len(maxoftwo) * 100)
+    df['avgofmaxtwopercentage'] = ((sum(maxoftwo.values()) / len(maxoftwo)) * 100)
     df['toxicityeval'] = df['avgofmaxtwopercentage'].apply(lambda x: 1 if x > 90 else 0) 
     return df
 
@@ -51,7 +50,7 @@ def predicttoxicity(df, texts_col, modelname='unbiased'):
       # get two higly toxic 
       maxtwotoxic = dict(Counter(result).most_common(2))
       result['maxtwotoxic'] = maxtwotoxic 
-      result['avgofmaxtwopercentage']= '{:.2f}%'.format(sum(maxtwotoxic.values()) / len(maxtwotoxic) * 100)
+      result['avgofmaxtwopercentage']= '{:.2f}%'.format((sum(maxtwotoxic.values()) / len(maxtwotoxic))* 100)
       responsedict[sentence] = result
       rdf = pd.DataFrame(list(responsedict.items()), columns=['Question', 'Response'])
       flatten_df = pd.json_normalize(rdf.to_dict(orient='records'))
